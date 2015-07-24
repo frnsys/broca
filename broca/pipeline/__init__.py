10dd11e1976d7f57d5b830bcb00b19eb7de55d6a
Francis Tseng <f+accounts@frnsys.com>
2015-07-23 20:45:30 -0400

    multi-pipeline support added

--- broca/pipeline/__init__.py
+++ broca/pipeline/__init__.py
@@ -1,20 +1,42 @@
+from itertools import product
+
+
 class Pipeline():
     def __init__(self, *pipes):
-        self.pipes = pipes
+        # If any of the pipes is a list, we are building multiple pipelines
+        if any(isinstance(p, list) for p in pipes):
+            # Coerce all pipes to lists
+            pipes = [p if isinstance(p, list) else [p] for p in pipes]
 
-        # Validate the pipeline
-        for p_out, p_in in zip(pipes, pipes[1:]):
-            if p_out.output != p_in.input:
-                raise Exception('Incompatible: pipe <{}> outputs <{}>, pipe <{}> requires input of <{}>.'.format(
-                    type(p_out).__name__, p_out.output,
-                    type(p_in).__name__, p_in.input
-                ))
+            # Build each pipeline
+            self.pipelines = [Pipeline(*pipes_) for pipes_ in product(*pipes)]
+
+        else:
+            self.pipes = pipes
+
+            # Validate the pipeline
+            for p_out, p_in in zip(pipes, pipes[1:]):
+                if p_out.output != p_in.input:
+                    raise Exception('Incompatible: pipe <{}> outputs <{}>, pipe <{}> requires input of <{}>.'.format(
+                        type(p_out).__name__, p_out.output,
+                        type(p_in).__name__, p_in.input
+                    ))
 
     def __call__(self, input):
-        for pipe in self.pipes:
-            output = pipe(input)
-            input = output
-        return output
+        if hasattr(self, 'pipelines'):
+            return [p(input) for p in self.pipelines]
+        else:
+            for pipe in self.pipes:
+                output = pipe(input)
+                input = output
+            return output
+
+
+    def __repr__(self):
+        if hasattr(self, 'pipelines'):
+            return 'Multi-Pipeline: {}'.format(' || '.join([str(p) for p in self.pipelines]))
+        else:
+            return ' -> '.join([type(p).__name__ for p in self.pipes])
 
 
 class PipeType():
